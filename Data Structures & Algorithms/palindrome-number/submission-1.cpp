0af7c581580c604cb8e6200cda9f256c0cbcc6ce
Sushant Jawale <sushantjawale15@gmail.com>
2026-05-24 18:18:56 +0530

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int revNum(int n)
+    {   int lt = 0;
+        int revNum = 0;
+        while(n > 0)
+        {
+            lt = n % 10;
+            revNum = revNum*10 + lt;
+            n = n / 10;
+        }
+        return revNum;
+    }
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int temp = x;
+        int rev = revNum(x);
+        if(temp == rev) return true;
+        return false;
+    }
+};
